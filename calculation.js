5233f6bb8c80c02ba28573e9cd4368515edc82ce
mdshakil20 <shakilislam737@gmail.com>
2022-08-22 21:18:55 +0600

    all calculation done

--- calculation.js
+++ calculation.js
@@ -1,5 +1,7 @@
 
 let child = 0;
+
+//this function selecting only 5 players
 function getAndPutName(idName) {
     const playerName = document.getElementById(idName).innerText;
     const li = document.createElement('li');
@@ -14,55 +16,81 @@ function getAndPutName(idName) {
         PlayerListParent.appendChild(li);
         child++;
     }
+    console.log(child);
+
 }
 
+//button disable  function
+function btnDisable(btnId) {
+    document.getElementById(btnId).disabled = true;
+    document.getElementById(btnId).style.backgroundColor = '#97999c';
+}
+
+//get input value
+function getInputValue(InputFieldId) {
+    const inputValueString = document.getElementById(InputFieldId).value;
+    const inputValue = parseFloat(inputValueString);
+    document.getElementById(InputFieldId).value = '';
+    return inputValue;
+}
+
+
+
+
+//when select1 button is clicked
 document.getElementById('selectBtn1').addEventListener('click', function () {
     if (child < 5) {
         getAndPutName('player1');
-        document.getElementById("selectBtn1").disabled = true;
-        document.getElementById("selectBtn1").style.backgroundColor = '#97999c';
+        btnDisable('selectBtn1');
     }
 });
-
+//when select2 button is clicked
 document.getElementById('selectBtn2').addEventListener('click', function () {
     if (child < 5) {
         getAndPutName('player2');
-        document.getElementById("selectBtn2").disabled = true;
-        document.getElementById("selectBtn2").style.backgroundColor = '#97999c';
+        btnDisable('selectBtn2');
     }
 });
-
+//when select3 button is clicked
 document.getElementById('selectBtn3').addEventListener('click', function () {
     if (child < 5) {
         getAndPutName('player3');
-        document.getElementById("selectBtn3").disabled = true;
-        document.getElementById("selectBtn3").style.backgroundColor = '#97999c';
+        btnDisable('selectBtn3');
     }
 });
-
+//when select4 button is clicked
 document.getElementById('selectBtn4').addEventListener('click', function () {
     if (child < 5) {
         getAndPutName('player4');
-        document.getElementById("selectBtn4").disabled = true;
-        document.getElementById("selectBtn4").style.backgroundColor = '#97999c';
+        btnDisable('selectBtn4');
     }
 });
-
+//when select5 button is clicked
 document.getElementById('selectBtn5').addEventListener('click', function () {
     if (child < 5) {
         getAndPutName('player5');
-        document.getElementById("selectBtn5").disabled = true;
-        document.getElementById("selectBtn5").style.backgroundColor = '#97999c';
+        btnDisable('selectBtn5');
     }
 });
-
+//when select6 button is clicked
 document.getElementById('selectBtn6').addEventListener('click', function () {
     if (child < 5) {
         getAndPutName('player6');
-        document.getElementById("selectBtn6").disabled = true;
-        document.getElementById("selectBtn6").style.backgroundColor = '#97999c';
+        btnDisable('selectBtn6');
     }
 });
 
+document.getElementById('calculate').addEventListener('click', function () {
+    const perPlayerCost = getInputValue('perPlayer');
+    const playersExpense = perPlayerCost * child;
+    document.getElementById('playerExpense').innerText = playersExpense;
+});
 
-
+document.getElementById('total_calculate').addEventListener('click', function () {
+    const managerCost = getInputValue('managerInputField');
+    const coachCost = getInputValue('coachInputField');
+    const totalPlayersCostSting =  document.getElementById('playerExpense').innerText;
+    const totalPlayersCost = parseFloat(totalPlayersCostSting);
+    const total = managerCost + coachCost + totalPlayersCost;
+    document.getElementById('total_expense').innerText = total;
+});
