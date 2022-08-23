c8c2673015d6f10628d2e9d12356187263f23f40
mdshakil20 <shakilislam737@gmail.com>
2022-08-23 07:35:59 +0600

    modified some code

--- calculation.js
+++ calculation.js
@@ -1,30 +1,41 @@
 
 let child = 0;
 
-//this function selecting only 5 players
-function getAndPutName(idName) {
-    const playerName = document.getElementById(idName).innerText;
-    const li = document.createElement('li');
-    li.innerText = playerName;
-    const PlayerListParent = document.getElementById('selectedPlayers');
-    if (PlayerListParent.childElementCount == 4) {
-        child++;
-        PlayerListParent.appendChild(li);
-        document.getElementById('hideElement').style.visibility = 'visible';
-    }
-    else {
-        PlayerListParent.appendChild(li);
-        child++;
-    }
-    console.log(child);
-
-}
 
 //button disable  function
 function btnDisable(btnId) {
     document.getElementById(btnId).disabled = true;
     document.getElementById(btnId).style.backgroundColor = '#97999c';
 }
+
+//this function selecting only 5 players
+function getAndPutName(playerNameId, btnId) {
+    const playerName = document.getElementById(playerNameId).innerText;
+    const li = document.createElement('li');
+    li.innerText = playerName;
+    const PlayerListParent = document.getElementById('selectedPlayers');
+
+    if (PlayerListParent.childElementCount == 4) {
+        PlayerListParent.appendChild(li);
+        document.getElementById('hideElement').style.visibility = 'visible';
+        btnDisable(btnId);
+        child++;
+    }
+    else if(PlayerListParent.childElementCount >= 5){
+        alert("You already selecte 5 players. You can't select any more.");
+        
+    }
+    if (PlayerListParent.childElementCount < 4){
+        PlayerListParent.appendChild(li);
+        btnDisable(btnId);
+        child++;
+    }
+    console.log(child);
+
+}
+
+
+
 
 //get input value
 function getInputValue(InputFieldId) {
@@ -39,45 +50,28 @@ function getInputValue(InputFieldId) {
 
 //when select1 button is clicked
 document.getElementById('selectBtn1').addEventListener('click', function () {
-    if (child < 5) {
-        getAndPutName('player1');
-        btnDisable('selectBtn1');
-    }
+    getAndPutName('player1','selectBtn1');
+   
 });
 //when select2 button is clicked
 document.getElementById('selectBtn2').addEventListener('click', function () {
-    if (child < 5) {
-        getAndPutName('player2');
-        btnDisable('selectBtn2');
-    }
+    getAndPutName('player2','selectBtn2');
 });
 //when select3 button is clicked
 document.getElementById('selectBtn3').addEventListener('click', function () {
-    if (child < 5) {
-        getAndPutName('player3');
-        btnDisable('selectBtn3');
-    }
+   getAndPutName('player3','selectBtn3');
 });
 //when select4 button is clicked
 document.getElementById('selectBtn4').addEventListener('click', function () {
-    if (child < 5) {
-        getAndPutName('player4');
-        btnDisable('selectBtn4');
-    }
+    getAndPutName('player4','selectBtn4');
 });
 //when select5 button is clicked
 document.getElementById('selectBtn5').addEventListener('click', function () {
-    if (child < 5) {
-        getAndPutName('player5');
-        btnDisable('selectBtn5');
-    }
+    getAndPutName('player5','selectBtn5');
 });
 //when select6 button is clicked
 document.getElementById('selectBtn6').addEventListener('click', function () {
-    if (child < 5) {
-        getAndPutName('player6');
-        btnDisable('selectBtn6');
-    }
+    getAndPutName('player6','selectBtn6');
 });
 
 document.getElementById('calculate').addEventListener('click', function () {
