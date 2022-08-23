ab7512c08cbad0780106c0e775d201563c67c64e
mdshakil20 <shakilislam737@gmail.com>
2022-08-23 07:40:05 +0600

    little bit code rearrange and added some comment

--- calculation.js
+++ calculation.js
@@ -15,7 +15,12 @@ function getAndPutName(playerNameId, btnId) {
     li.innerText = playerName;
     const PlayerListParent = document.getElementById('selectedPlayers');
 
-    if (PlayerListParent.childElementCount == 4) {
+    if (PlayerListParent.childElementCount < 4){
+        PlayerListParent.appendChild(li);
+        btnDisable(btnId);
+        child++;
+    }
+    else if (PlayerListParent.childElementCount == 4) {
         PlayerListParent.appendChild(li);
         document.getElementById('hideElement').style.visibility = 'visible';
         btnDisable(btnId);
@@ -23,14 +28,7 @@ function getAndPutName(playerNameId, btnId) {
     }
     else if(PlayerListParent.childElementCount >= 5){
         alert("You already selecte 5 players. You can't select any more.");
-        
     }
-    if (PlayerListParent.childElementCount < 4){
-        PlayerListParent.appendChild(li);
-        btnDisable(btnId);
-        child++;
-    }
-    console.log(child);
 
 }
 
@@ -45,41 +43,44 @@ function getInputValue(InputFieldId) {
     return inputValue;
 }
 
-
-
-
 //when select1 button is clicked
 document.getElementById('selectBtn1').addEventListener('click', function () {
     getAndPutName('player1','selectBtn1');
-   
 });
+
 //when select2 button is clicked
 document.getElementById('selectBtn2').addEventListener('click', function () {
     getAndPutName('player2','selectBtn2');
 });
+
 //when select3 button is clicked
 document.getElementById('selectBtn3').addEventListener('click', function () {
    getAndPutName('player3','selectBtn3');
 });
+
 //when select4 button is clicked
 document.getElementById('selectBtn4').addEventListener('click', function () {
     getAndPutName('player4','selectBtn4');
 });
+
 //when select5 button is clicked
 document.getElementById('selectBtn5').addEventListener('click', function () {
     getAndPutName('player5','selectBtn5');
 });
+
 //when select6 button is clicked
 document.getElementById('selectBtn6').addEventListener('click', function () {
     getAndPutName('player6','selectBtn6');
 });
 
+//total player cost calculating and put the value
 document.getElementById('calculate').addEventListener('click', function () {
     const perPlayerCost = getInputValue('perPlayer');
     const playersExpense = perPlayerCost * child;
     document.getElementById('playerExpense').innerText = playersExpense;
 });
 
+//total cost calculating and put the value
 document.getElementById('total_calculate').addEventListener('click', function () {
     const managerCost = getInputValue('managerInputField');
     const coachCost = getInputValue('coachInputField');
